08f3be618c3e5f92e89b6c1a048e74aacdfc07ab
maxam-hacker <maxam.hacker@gmail.com>
2025-01-27 16:15:14 +0300

    the same

--- internal/hashcash/hashcash_test.go
+++ internal/hashcash/hashcash_test.go
@@ -29,3 +29,43 @@ func TestOnHashcash(t *testing.T) {
 	checked := hc.Check(sha)
 	assert.Equal(t, true, checked)
 }
+
+func TestOnHashcashWith3Zeros(t *testing.T) {
+	var err error
+
+	hc := Hashcash{
+		Version:  1,
+		Zeros:    3,
+		Date:     123456,
+		Resource: "maxam.hacker@gmail.com",
+		Counter:  101,
+		Rand:     "dgbernler",
+	}
+
+	hc, err = hc.Compute(1000000)
+	assert.NoError(t, err)
+	assert.Equal(t, "1:3:123456:maxam.hacker@gmail.com::dgbernler:2028", hc.ToString())
+
+	sha, err := hc.GetSha1()
+	assert.NoError(t, err)
+	assert.Equal(t, "0006b095a1b57cf653088b05de7c5d48a847c9c6", sha)
+
+	checked := hc.Check(sha)
+	assert.Equal(t, true, checked)
+}
+
+func TestOnHashcashWith3ZerosAndError(t *testing.T) {
+	var err error
+
+	hc := Hashcash{
+		Version:  1,
+		Zeros:    3,
+		Date:     123456,
+		Resource: "maxam.hacker@gmail.com",
+		Counter:  101,
+		Rand:     "dgbernler",
+	}
+
+	_, err = hc.Compute(1000)
+	assert.Error(t, err)
+}

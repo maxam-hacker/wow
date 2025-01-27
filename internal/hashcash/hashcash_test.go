d7bee94a631fde0d9a33a4b7392fe3acac37fae3
maxam-hacker <maxam.hacker@gmail.com>
2025-01-27 18:21:56 +0300

    fix for tests... need to improve server side hash init

--- internal/hashcash/hashcash_test.go
+++ internal/hashcash/hashcash_test.go
@@ -18,15 +18,15 @@ func TestOnHashcash(t *testing.T) {
 		Rand:     "dgbernler",
 	}
 
-	hc, err = hc.Compute(1000)
+	computedHc, err := hc.Compute(1000)
 	assert.NoError(t, err)
-	assert.Equal(t, "1:1:123456:maxam.hacker@gmail.com::dgbernler:102", hc.ToString())
+	assert.Equal(t, "1:1:123456:maxam.hacker@gmail.com::dgbernler:101", hc.ToString())
 
-	sha, err := hc.GetSha1()
+	sha, err := computedHc.GetSha1()
 	assert.NoError(t, err)
 	assert.Equal(t, "0d31fe344c1c0dc998fccade6c7b290044208353", sha)
 
-	checked := hc.Check(sha)
+	checked := computedHc.Check(sha)
 	assert.Equal(t, true, checked)
 }
 
@@ -42,15 +42,15 @@ func TestOnHashcashWith3Zeros(t *testing.T) {
 		Rand:     "dgbernler",
 	}
 
-	hc, err = hc.Compute(1000000)
+	computedHc, err := hc.Compute(1000000)
 	assert.NoError(t, err)
-	assert.Equal(t, "1:3:123456:maxam.hacker@gmail.com::dgbernler:2028", hc.ToString())
+	assert.Equal(t, "1:3:123456:maxam.hacker@gmail.com::dgbernler:2028", computedHc.ToString())
 
-	sha, err := hc.GetSha1()
+	sha, err := computedHc.GetSha1()
 	assert.NoError(t, err)
 	assert.Equal(t, "0006b095a1b57cf653088b05de7c5d48a847c9c6", sha)
 
-	checked := hc.Check(sha)
+	checked := computedHc.Check(sha)
 	assert.Equal(t, true, checked)
 }
 
@@ -66,6 +66,7 @@ func TestOnHashcashWith3ZerosAndError(t *testing.T) {
 		Rand:     "dgbernler",
 	}
 
-	_, err = hc.Compute(1000)
+	computedHc, err := hc.Compute(1000)
 	assert.Error(t, err)
+	assert.Equal(t, 1001, computedHc.Counter)
 }
